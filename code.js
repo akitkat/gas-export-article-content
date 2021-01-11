8318af539efeb419bddfca8329d6f48dd2fcaad5
Naoya Akimoto <akitkat.cook.dx@gmail.com>
2021-01-11 13:19:36 +0900

    refine content, add test method.

--- code.js
+++ code.js
@@ -1,27 +1,37 @@
 const doGet = e => {
-  const ids = e.parameter.ids.split`,`
-  const sql = SpreadSheetsSQL.open(SpreadsheetApp.getActiveSpreadsheet().getId(), 'common')
-  const data = sql.select(['No.', 'title', 'content', 'instagram']).filter(`No. IN ${ids}`).result()
+    if (e.parameter.test !== undefined) {
+        return test(e)
+    }
 
-  let res = `<div class="toc_container toc_white no_bullets"><p class="toc_title">目次</p><ul class="toc_list">`;
-  for (let i in data) {
-    res += `
-        <li>
-            <a href="#${Number(i) + 1}">
-                <span class="toc_number toc_depth_${Number(i) + 1}">${Number(i) + 1}</span>
-                ${e.parameter.title}${Number(i) + 1}：${data[i]['title']}
-            </a>
-        </li>
-    `;
-  }
-  res += `</ul></div>`;
+    const post_id = e.parameter.post_id
+    const spreadsheet_id = SpreadsheetApp.getActiveSpreadsheet().getId()
+    const settings = SpreadSheetsSQL.open(spreadsheet_id, 'article_ids').select(['post_id', 'ids', 'table_text']).filter(`post_id = ${post_id}`).result()
+    const data = SpreadSheetsSQL.open(spreadsheet_id, 'common').select(['No.', 'title', 'content', 'instagram']).filter(`No. IN ${settings[0]['ids'].split(',')}`).result()
 
-  for (let i in data) {
-    res += `
-      <h2 id="${Number(i) + 1}">${e.parameter.title}${Number(i) + 1}: ${data[i]['title']}</h2>
-      <p class="well3">${data[i]['content']}</p>
-      ${data[i]['instagram']}
-    `
-  }
-  return ContentService.createTextOutput(res).setMimeType(ContentService.MimeType.TEXT);
+    let res = `<div class="toc_container toc_white no_bullets"><p class="toc_title">目次</p><ul class="toc_list">`;
+    for (let i in data) {
+        res += `
+            <li>
+                <a href="#${Number(i) + 1}">
+                    <span class="toc_number toc_depth_${Number(i) + 1}">${Number(i) + 1}</span>
+                    ${settings[0]['table_text']}${Number(i) + 1}：${data[i]['title']}
+                </a>
+            </li>
+        `;
+    }
+    res += `</ul></div>`;
+
+    for (let i in data) {
+        res += `
+            <h2 id="${Number(i) + 1}">${settings[0]['table_text']}${Number(i) + 1}: ${data[i]['title']}</h2>
+            <p class="well3">${data[i]['content']}</p>
+            ${data[i]['instagram']}
+        `
+    }
+    return ContentService.createTextOutput(res).setMimeType(ContentService.MimeType.TEXT);
+}
+
+const test = e => {
+    let res = ''
+    return ContentService.createTextOutput(res).setMimeType(ContentService.MimeType.TEXT);
 }
